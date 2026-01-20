6bf83f460b631b4f302a5df9911d123dbfc048a0
N3T0 <starkodersamm90210@gmail.com>
2026-01-20 15:25:16 -0500

    CartItems component

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useSelector, useDispatch } from 'react-redux';
 import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
